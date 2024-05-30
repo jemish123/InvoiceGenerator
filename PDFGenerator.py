20ebe94cce157883999809f083571b11c2eea49b
jemish123 <jemishgm103@gmail.com>
2024-05-31 00:03:43 +0530

    Initial commit

---
+++ PDFGenerator.py
@@ -0,0 +1,23 @@
+import jinja2
+import pdfkit
+from datetime import datetime
+
+my_name = "Jemish Mangukiya"
+
+item1 = "TV"
+item2 = "Laptop"
+item3 = "Washing Machine"
+today_date = datetime.today().strftime("%d %b %Y")
+
+context = {'my_name': my_name, 'item1': item1, 'item2': item2, 'item3': item3,
+           'today_date': today_date}
+
+template_loader = jinja2.FileSystemLoader('template_html/')
+template_env = jinja2.Environment(loader=template_loader)
+
+template = template_env.get_template('pdf_file.html')
+output_text = template.render(context)
+
+config = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
+pdfkit.from_string(output_text, 'pdf_generated.pdf', configuration=config)
+pf
